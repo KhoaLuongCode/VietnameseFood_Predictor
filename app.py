720eff574a85a12801d1d5111accaed3434901ac
Khoa Luong <khoaluong@Khoas-MacBook-Pro.local>
2024-01-21 23:26:24 -0500

    final changes

--- app.py
+++ app.py
@@ -33,13 +33,14 @@ class Item(BaseModel):
     Vegetarian: str
 
 
-app.mount("/", StaticFiles(directory="my-app/build"), name="static")
 
 
 # test
-# @app.get('/')
-# async def index():
-#    return RedirectResponse(url='/index.html')
+@app.get('/')
+async def index():
+    return RedirectResponse(url='/index.html')
+
+app.mount("/", StaticFiles(directory="build"), name="static")
 
 
 @app.post('/predict')
